cf42e9743fd33595d4ec092b75df2f724bda7538
Narendra Singh <ns1039492@gmail.com>
2025-05-01 09:48:00 +0530

    update backend

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 async function dbConn(){
-    const conn=await mongoose.connect('mongodb://localhost:27017/studentapp');
+    const conn=await mongoose.connect('mongodb+srv://Narendra2005:root@cluster0.9sc1wnx.mongodb.net/crud-live?retryWrites=true&w=majority&appName=Cluster0');
     if(conn){
         console.log('database connect successfully')
 
